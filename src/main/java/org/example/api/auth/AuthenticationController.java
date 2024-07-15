9379b134369bf7379a56e25812a094c2f3b5b7ed
INTELIGENCIA <darodriguesufc@gmail.com>
2024-07-15 16:45:25 -0300

    commit inicial

--- src/main/java/org/example/api/auth/AuthenticationController.java
+++ src/main/java/org/example/api/auth/AuthenticationController.java
@@ -40,21 +40,30 @@ public class AuthenticationController {
 
     @PostMapping("/register")
     public ResponseEntity register(@RequestBody @Valid RegisterDTO data){
-        if(this.repository.findByUsername(data.username()) != null) return ResponseEntity.badRequest().build();
+        if(this.repository.findByUsername(data.username()) != null) return ResponseEntity.badRequest().body("User already exists");
 
-        String encryptedPassword = new BCryptPasswordEncoder().encode(data.password());
-        User newUser = new User(data.username(), encryptedPassword, data.role());
+        try{
+            String encryptedPassword = new BCryptPasswordEncoder().encode(data.password());
+            User newUser = new User(data.username(), encryptedPassword, data.role());
 
-        this.repository.save(newUser);
+            this.repository.save(newUser);
 
-        return ResponseEntity.ok().build();
+            return ResponseEntity.ok().body(newUser);
+        }catch(Exception e){
+            return ResponseEntity.badRequest().body(e);
+        }
     }
 
     @PutMapping("/change_user")
     public ResponseEntity edit(@RequestBody @Valid RegisterDTO data){
-        String encryptedPassword = new BCryptPasswordEncoder().encode(data.password());
-        this.repository.save(new User(data.username(),encryptedPassword,data.role()));
-        return ResponseEntity.ok().build();
+        try{
+            String encryptedPassword = new BCryptPasswordEncoder().encode(data.password());
+            this.repository.save(new User(data.username(),encryptedPassword,data.role()));
+            return ResponseEntity.ok().body((new User(data.username(),encryptedPassword,data.role())));
+        }catch(Exception e){
+            return ResponseEntity.badRequest().body(e);
+        }
+
     }
 
 }
